4feea189ff15115494c5b79cf3dd7a6858bc6b32
ManuelCebreiro <cebreirom@gmail.com>
2022-08-09 14:25:46 +0000

    listo

--- src/main.py
+++ src/main.py
@@ -31,39 +31,150 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+#TODOS LOS ENDPOINTS DE USERS
+
 @app.route('/user', methods=['GET'])
 def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
-    return jsonify(response_body), 200
+    user = [x.serialize() for x in User.query.all()]
+    return jsonify(user), 200
 
+#LLAMAR A TODOS LOS USUARIOS
+
+@app.route('/favoritos/<int:user_id>', methods=['GET'])
+def favoritesUser(user_id):
+    user1 = User.query.get(user_id)
+    person = [x.serialize() for x in user1.peoplefavoritos]
+    planet =  [i.serialize() for i in user1.planetsfavoritos]
+    vehicle = [y.serialize() for y in user1.vehiclesfavoritos]
+    allfavorites = [ person + planet + vehicle]
+
+    return jsonify(allfavorites), 200
+
+
+#LLAMAR A LOS FAVORITOS DE UN USUARIO
+
+@app.route('/personaje/<int:user_id>/<int:person_id>', methods=['POST'])
+def addPersonFavorite(person_id, user_id):
+    user1 = User.query.get(user_id)
+    person = People.query.get(person_id)
+
+    if person in user1.peoplefavoritos:
+        return "Este personaje ya está en favoritos", 400
+    else:
+        user1.peoplefavoritos.append(person)
+        db.session.commit()
+        return "Personaje añadido", 200
+
+#AÑADIR PERSONAJE FAVORITO
+
+@app.route('/personaje/<int:user_id>/<int:person_id>', methods=['DELETE'])
+def deletePersonFavorite(person_id, user_id):
+    user1 = User.query.get(user_id)
+    person = People.query.get(person_id)
+
+    if person not in user1.peoplefavoritos:
+        return "Este personaje ya no está en favoritos", 400
+    else:
+        user1.peoplefavoritos.remove(person)
+        db.session.commit()
+        return "Personaje eliminado", 200
+
+#ELIMINAR PERSONAJE FAVORITO
+
+@app.route('/planet/<int:user_id>/<int:planet_id>', methods=['POST'])
+def addPlanetFavorite(planet_id, user_id):
+    user1 = User.query.get(user_id)
+    planet = Planets.query.get(planet_id)
+
+    if planet in user1.planetsfavoritos:
+        return "Este planeta ya está en favoritos", 400
+    else:
+        user1.planetsfavoritos.append(planet)
+        db.session.commit()
+        return "Planeta añadido", 200
+
+#AÑADIR PLANETA FAVORITO
+
+@app.route('/planet/<int:user_id>/<int:planet_id>', methods=['DELETE'])
+def deletePlanetFavorite(planet_id, user_id):
+    user1 = User.query.get(user_id)
+    planet = Planets.query.get(planet_id)
+
+    if planet not in user1.planetsfavoritos:
+        return "Este planeta ya no está en favoritos", 400
+    else:
+        user1.planetsfavoritos.remove(planet)
+        db.session.commit()
+        return "Planeta eliminado", 200
+
+#ELIMINAR PLANETA FAVORITO
+
+@app.route('/vehicles/<int:user_id>/<int:vehicles_id>', methods=['POST'])
+def addVehiclesFavorite(vehicles_id, user_id):
+    user1 = User.query.get(user_id)
+    vehicles = Vehicles.query.get(vehicles_id)
+
+    if vehicles in user1.vehiclesfavoritos:
+        return "Este vehiculo ya está en favoritos", 400
+    else:
+        user1.vehiclesfavoritos.append(vehicles)
+        db.session.commit()
+        return "Vehiculo añadido", 200
+
+#AÑADIR VEHICLES FAVORITO
+
+@app.route('/vehicles/<int:user_id>/<int:vehicles_id>', methods=['DELETE'])
+def deleteVehiclesFavorite(vehicles_id, user_id):
+    user1 = User.query.get(user_id)
+    vehicles = Vehicles.query.get(vehicles_id)
+
+    if vehicles not in user1.vehiclesfavoritos:
+        return "Este vehiculo ya no está en favoritos", 400
+    else:
+        user1.vehiclesfavoritos.remove(vehicles)
+        db.session.commit()
+        return "Vehiculo eliminado", 200
+
+#ELIMINAR PLANETA FAVORITO
+
+#TODOS LOS ENDPOINTS DE PEOPLE
 
 @app.route('/people', methods=['GET'])
 def getPeople():
-
     people = People.query.all()
-
     return jsonify([person.serialize() for person in people]), 200
 
-# this only runs if `$ python src/main.py` is executed
-if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT, debug=False)
+#LLAMAR A TODOS LOS PERSONAJES
+
+@app.route('/people/<int:people_id>', methods=['GET'])
+def getPeopleSingle(people_id):
+
+    # user1 = People.query.get(people_id)
+    user1 = People.query.filter_by(id = people_id).first()
+    return jsonify(user1.serialize())
+
+#LLAMAR A UN PERSONAJE INDIVIUAL
+
+#TODOS LOS ENDPOINTS DE PLANETS
 
 @app.route('/planets', methods=['GET'])
 def getPlanets():
 
     planets = Planets.query.all()
-
     return jsonify([planet.serialize() for planet in planets]), 200
 
-# this only runs if `$ python src/main.py` is executed
-if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT, debug=False)
+#LLAMAR A TODOS LOS PLANETAS
 
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def getPlanetSingle(planet_id):
+
+    user1 = Planets.query.filter_by(id = planet_id).first()
+    return jsonify(user1.serialize())
+
+#LLAMAR A UN PLANETA INDIVIDUAL
+
+#TODOS LOS ENDPOINTS DE VEHICLES
 
 @app.route('/vehicles', methods=['GET'])
 def getVehicles():
@@ -71,6 +182,25 @@ def getVehicles():
     vehicles = Vehicles.query.all()
 
     return jsonify([vehicle.serialize() for vehicle in vehicles]), 200
+
+#LLAMAR A TODOS LOS VEHICULOS
+
+@app.route('/vehicles/<int:vehicle_id>', methods=['GET'])
+def getVehiclesSingle(vehicle_id):
+
+    user1 = Vehicles.query.filter_by(id = vehicle_id).first()
+    return jsonify(user1.serialize())
+
+#LLAMAR A VEHICULOS INDIVIDUAL
+
+
+
+
+
+
+
+
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -32,7 +32,8 @@ class User(db.Model):
 
     def serialize(self):
         return {
-            "username": self.name,
+            "id": self.id,
+            "name": self.name,
             "email": self.email
         }
 
@@ -40,9 +41,9 @@ class User(db.Model):
 class People(db.Model):
     id = db.Column(db.Integer, primary_key=True,)
     name = db.Column(db.String(250))
-    birthday = db.Column(db.String(250))
+    birthday = db.Column(db.Integer)
     gender = db.Column(db.String(250))
-    height = db.Column(db.String(250))
+    height = db.Column(db.Integer)
     skin_color = db.Column(db.String(250))
     eye_color = db.Column(db.String(250))
 
@@ -51,29 +52,30 @@ class People(db.Model):
 
     def serialize(self):
         return {
+            "id": self.id,
             "username": self.name,
-            "email": self.email,
             "birthday": self.birthday,
             "gender": self.gender,
             "height": self.height,
             "skin_color": self.skin_color,
-            "eye_color": self.eye_color
+            "eye_color": self.eye_color,
+            # "homeplanet": self.homeplanet.name
         }
-
 class Planets(db.Model):
     id = db.Column(db.Integer, primary_key=True,)
     name = db.Column(db.String(250))
-    population = db.Column(db.String(250))
+    population = db.Column(db.Integer)
     climate = db.Column(db.String(250))
     terrain = db.Column(db.String(250))
-    rotation_period = db.Column(db.String(250))
-    orbital_period = db.Column(db.String(250))
+    rotation_period = db.Column(db.Integer)
+    orbital_period = db.Column(db.Integer)
 
     def __repr__(self):
         return '<Planet %r>' % self.name
 
     def serialize(self):
         return {
+            "id": self.id,
             "username": self.name,
             "population": self.population,
             "climate": self.climate,
@@ -89,13 +91,14 @@ class Vehicles(db.Model):
     vehicles_class = db.Column(db.String(250))
     length = db.Column(db.String(250))
     cargo_capacity = db.Column(db.String(250))
-    max_Speed = db.Column(db.String(250))
+    max_Speed = db.Column(db.Integer)
     
     def __repr__(self):
         return '<Vehicle %r>' % self.name
 
     def serialize(self):
         return {
+            "id": self.id,
             "username": self.name,
             "model": self.model,
             "vehicles_class": self.vehicles_class,
